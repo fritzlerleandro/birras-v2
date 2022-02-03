93a9fd52bfc92b0ee909be043377980cf011f997
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2022-02-02 22:30:03 -0300

    feat(UI): add initial pages to show brands
    This allow to SSR the page for brands and specific brand

---
+++ src/pages/brands/index.tsx
@@ -0,0 +1,39 @@
+import { definitions } from 'types/supabase';
+import { supabase } from  'utils/supabaseClient'
+
+export async function getServerSideProps () {
+    const { data: brands, error } = await supabase
+      .from<definitions['beer_brands']>('beer_brands')
+      .select('*')
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+
+    return {
+        props: { 
+            brands,
+         }
+    }
+}
+
+export default function Brands({ brands }) {
+// console.log("brands.lenght and itself", brands.length, brands)
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      { brands ? brands.map((brand) => {
+          return (
+            <div style={{height: '10px'}} key={brand.id}>
+                <h2>{brand.brand_name}</h2>
+                <p>{brand.logo_url}</p>
+            </div>
+          )
+          }) : <div className="loader">
+               Loading...
+          </div>
+    }
+    </div>
+  )
+}
