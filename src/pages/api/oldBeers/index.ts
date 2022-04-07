6d55d83b507d74cced7d277cc59c028387f94b5c
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2022-04-06 22:24:14 -0300

    feat (api): implement endpoint to get old beer list from spreadhseet

---
+++ src/pages/api/oldBeers/index.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import oldBeers from 'utils/oldBeers'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  const getOldBeers = async () => {
+    try {
+        return res.status(200).json(oldBeers)
+    } catch (error) {
+        return res.status(error.status).json({ error })
+    }
+  }
+
+
+  switch(method) {
+    case 'GET':
+      // Get list of beers
+      getOldBeers();
+      break
+    case 'POST':
+      // Create new beer
+      break
+    default:
+      res.status(405).end() //Method Not Allowed
+      break
+  }
+
+
+}
