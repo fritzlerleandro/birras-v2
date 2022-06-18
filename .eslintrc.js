42f91b85234674376e36647ea38886d6baed970d
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2022-06-18 16:09:40 -0300

    fix(lint): update eslint config with prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,79 @@
 module.exports = {
-"root": true,
-  "extends": [
-    "react-app",
-    "plugin:prettier/recommended",
-    "prettier/react"
-  ],
-  "plugins": ["react-hooks"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json",
-    "tsconfigRootDir": __dirname
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
+  extends: ["plugin:prettier/recommended", "next", "next/core-web-vitals"],
+  plugins: ["react", "prettier", "import", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  "rules": {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
     "no-console": "warn",
-    "no-unused-vars": [
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": [
       "warn",
       {
-        "args": "after-used",
-        "ignoreRestSiblings": false,
-        "argsIgnorePattern": "^_$"
-      }
+        printWidth: 100,
+        trailingComma: "all",
+        tabWidth: 2,
+        semi: true,
+        singleQuote: false,
+        bracketSpacing: false,
+        arrowParens: "always",
+        endOfLine: "auto",
+      },
     ],
-    "prettier/prettier": [
-      "error",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
       {
-        "printWidth": 100,
-        "trailingComma": "all",
-        "tabWidth": 2,
-        "semi": true,
-        "singleQuote": false,
-        "bracketSpacing": false,
-        "arrowParens": "always"
-      }
+        args: "after-used",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "^_.*?$",
+      },
     ],
-    "import/order": ["warn", {"newlines-between": "always"}],
+    "import/order": [
+      "warn",
+      {
+        groups: ["type", "builtin", "object", "external", "internal", "parent", "sibling", "index"],
+        pathGroups: [
+          {
+            pattern: "~/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        "newlines-between": "always",
+      },
+    ],
     "react/self-closing-comp": "warn",
-    "react-hooks/rules-of-hooks": "warn",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/jsx-sort-props": ["warn", {
-      "callbacksLast": true,
-      "shorthandFirst": true,
-      "noSortAlphabetically": false,
-      "reservedFirst": true
-    }]
-  }
-}
-// {
-//   "extends": "next/core-web-vitals"
-// }
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+    "padding-line-between-statements": [
+      "warn",
+      {blankLine: "always", prev: "*", next: "return"},
+      {blankLine: "always", prev: ["const", "let", "var"], next: "*"},
+      {blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"]},
+    ],
+  },
+};
